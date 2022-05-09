2405c46a9d520c31bb171779a09ba7c27345a097
BouazziH <bouazzicdi@gmail.com>
2022-05-09 11:30:15 +0200

    declaration des variable

--- 01-syntaxe/index.php
+++ 01-syntaxe/index.php
@@ -4,6 +4,42 @@
 echo'test en php',' autre teste <br>';
 //print c'est le moin utiliser en php
 print' texte en php avec print <br>';
+// declaration variable
+$nom = 'Bouazzi';
+$age = 28;
+echo $nom,' ', $age;
+//differente maniere de declaration de variable
+$camelCase ='une variable en camel case';
+$snake_case = 'une variable en snake case';
+$pascaleCase = 'une variable pascale case';
+$var = 'une variable en kebab-case';
+//string
+$nom='abc';
+//nombre
+$int = 10;
+//float 
+$note=15.36;
+//bool
+$var=true;
+$var=false;
+//array/objet
+$tableau=['groupe',21,5,23,false];
+//null
+$var=null;
 
+// simple/double guillemets
+$message= "<br>Aujroud'hui";
+$mess ='aujourd\'hui';
+echo $mess;
+
+$txt = 'bonjou';
+echo '$txt tout le monde';
+echo "$txt tout le mondss";
+//concatenation
+$fruit1='kiwi';
+$fruit1='pomme';
+$fruit2='poire';
+
+echo '<br>'.$fruit1.' '.$fruit2;
 
 ?>
