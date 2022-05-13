379e6f57dd1e47326c5ad98305f315316ce8ca59
BouazziH <bouazzicdi@gmail.com>
2022-05-13 10:13:18 +0200

    i added links to my pages

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-phpinfo();
+//phpinfo();
 
 
 ?>
+<ul>
+    <li><a href="./01-syntaxe/index.php">Syntaxe</a></li>
+    <li><a href="./02-variables/index.php">Variables</a></li>
+    <li><a href="./03-tableaux/index.php">Tableaux</a></li>
+</ul>
