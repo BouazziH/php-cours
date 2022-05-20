fc2e53d53dcdc7a9ee01471d386182118a2063d2
BouazziH <bouazzicdi@gmail.com>
2022-05-20 11:44:20 +0200

    tavail-01

--- travaux/travail-01.php
+++ travaux/travail-01.php
@@ -12,5 +12,43 @@ Travail-01 :
 | 03 | 37.4 |
 | 35 | 95 |
 | 11 | 51.8 |
+C*9/5+32
+
  */
+echo '<table>';
+$f='';
+$trav=[25,03,35,11];
+//print_r($trav);
+echo '<tr>';
+echo '<td>|</td>';
+    echo'<td>°C</td>';
+    echo '<td>|</td>';
+    echo'<td>°F</td>';
+    echo '<td>|</td>';
+    echo '</tr>';
+    echo '<tr>';
+    echo '<td>|</td>';
+    echo'<td>---</td>';
+    echo '<td>|</td>';
+    echo'<td>---</td>';
+    echo '<td>|</td>';
+    echo '</tr>';
+for ($i=0; $i < count($trav) ; $i++) 
+{ 
+    
+$f=$trav[$i]*9/5+32;
+echo'<tr>';
+echo '<td>|</td>';
+echo '<td>'.$trav[$i].'</td>';
+echo '<td>|</td>';
+echo '<td>'.$f.'</td>';
+echo '<td>|</td>';
+echo '</tr>';
+
+
+
+} 
+
+echo '</table>';
+?>
 
